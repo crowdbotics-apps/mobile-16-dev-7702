d4fa37f699f86069554464aaf53644c6341e88d7
Crowdbotics <team@crowdbotics.com>
2020-07-16 07:39:58 +0000

    Update components

--- src/features/BlankScreen68987/screens/blank.js
+++ src/features/BlankScreen68987/screens/blank.js
@@ -61,36 +61,6 @@ export class _Blank extends React.Component {
         backgroundSize: "auto"
       }}
     >
-      <Text
-        style={{
-          marginLeft: 0,
-          marginRight: 0,
-          marginTop: 5,
-          marginBottom: 5,
-          paddingLeft: 5,
-          paddingRight: 5,
-          paddingTop: 5,
-          paddingBottom: 5,
-          overflow: "visible",
-          textAlign: "left",
-          verticalAlign: "baseline",
-          fontSize: 20,
-          color: "#000000",
-          backgroundColor: "#ffffff",
-          fontStyle: "normal",
-          fontWeight: "normal",
-          borderColor: "#000000",
-          borderStyle: "solid",
-          borderWidth: 0,
-          borderLeftWidth: 0,
-          borderRightWidth: 0,
-          borderTopWidth: 0,
-          borderBottomWidth: 0,
-          borderRadius: 0
-        }}
-      >
-        Sample text content
-      </Text>
       <Input
         placeholder="Sample text input placeholder"
         editable={true}
@@ -190,6 +160,36 @@ export class _Blank extends React.Component {
         }}
         date={this.state.Datepicker_5}
         onSelect={nextValue => this.setState({ Datepicker_5: nextValue })}
+      />
+      <Image
+        resizeMode="cover"
+        source={{
+          uri:
+            "https://d3nfddh2sbo9hk.cloudfront.net/media/project_component_resources/6_qzydNPM.jpg"
+        }}
+        style={{
+          width: "100%",
+          height: 200,
+          marginLeft: 0,
+          marginRight: 0,
+          marginTop: 0,
+          marginBottom: 0,
+          paddingLeft: 0,
+          paddingRight: 0,
+          paddingTop: 0,
+          paddingBottom: 0,
+          overflow: "visible",
+          textAlign: "left",
+          verticalAlign: "baseline",
+          borderColor: "#000000",
+          borderStyle: "solid",
+          borderWidth: 0,
+          borderLeftWidth: 0,
+          borderRightWidth: 0,
+          borderTopWidth: 0,
+          borderBottomWidth: 0,
+          borderRadius: 0
+        }}
       />
     </View>
   )
