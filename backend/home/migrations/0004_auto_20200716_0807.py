b212596289eca232ca18393a9f1026e1595e4cc1
Crowdbotics <team@crowdbotics.com>
2020-07-16 10:21:37 +0000

    Update models

--- backend/home/migrations/0004_auto_20200716_0807.py
+++ backend/home/migrations/0004_auto_20200716_0807.py
@@ -6,19 +6,24 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('home', '0003_customtext_hgfhgfhgfhg'),
+        ("home", "0003_customtext_hgfhgfhgfhg"),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='Thfjhfk',
+            name="Thfjhfk",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('fghedyjhetyje', models.BigIntegerField()),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("fghedyjhetyje", models.BigIntegerField()),
             ],
         ),
-        migrations.RemoveField(
-            model_name='customtext',
-            name='hgfhgfhgfhg',
-        ),
+        migrations.RemoveField(model_name="customtext", name="hgfhgfhgfhg",),
     ]
